c1eded5f18544f117a6926a07b91771b10254189
ddrieck <ddrieck@gmail.com>
2013-11-22 00:22:43 -0500

    Lab 6: Add jquery plugins

---
+++ coffee/application.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+    $('#slider').nivoSlider({
+   	directionNav: false,             // Next & Prev navigation
+    controlNav: false,               // 1,2,3... navigation
+    });
+});
