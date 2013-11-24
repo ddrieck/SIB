d3730c7613358e7cd7e4073300354358a7a81555
ddrieck <ddrieck@gmail.com>
2013-11-23 22:47:06 -0500

    Lab 11: Javascript and DOM manipulation. Stretch objective completed.

--- javascript/js/application.js
+++ javascript/js/application.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	//$(".product").find("img").fadeOut(2000);
 	$(".details").on("click", function(){
 		$(this).parent().find(".description").slideToggle();
+		return false;
 	});
 	$(".eu-toggle").on("click", function(){
 		$(".eu").show();
@@ -26,19 +27,34 @@ $(".addtocart").on("click", function(){
 	} else {
 		var priceOfProduct = productClass.find(".eu").text();
 	}
-
+	//Have to remove the currency symbol. Use slice to remove the symbol and parseFloat to convert it to a number.
 	priceOfProduct = priceOfProduct.slice(1,3);
 	priceOfProduct = parseFloat(priceOfProduct);
-	
+
+
 	var cartItem = { name: nameOfProduct,
-                 	sku: skuOfProduct, 
-                 	price: priceOfProduct, 
-                 	quantity: 1 };
+	             	sku: skuOfProduct, 
+	               	price: priceOfProduct, 
+	               	quantity: 1 };
+	
+	//Looping through an array of objects is a pain in the ass so use JSON method to convert to string
+	var cartArrayString = JSON.stringify(cart);
 
-	cart.push(cartItem);
+	//Use indexOf of the string to see if the object already exists in array. If so push cartItem.
+	if (cartArrayString.indexOf(skuOfProduct) === -1 || cart.length === 0){
+			cart.push(cartItem);
+	} else {
+		//Use grep to search array and find where sku matches button click. Add one to quantity of found object.
+		var cartSearch = $.grep(cart, function(n, i){
+			if (n.sku === skuOfProduct) {
+				n.quantity++;
+			}
+		});
+	}
 
+	
 	function refreshCart(){
-		total = 0;
+		total = 0;	
 		$("#cart").empty();
 
 		for(var i = 0; i < cart.length; i++) {
@@ -49,23 +65,29 @@ $(".addtocart").on("click", function(){
 	 			var sku = cartItem.sku;
 	 			var itemDiv = $("<div>").addClass("cart-item");
 
- 			if ($(".us").is(":visible")){
- 				$("#cart").append(itemDiv).append("$" + price + " ").append(quantity + " ").append(name + " ").append(sku + " ");
- 					} else {
- 					$("#cart").append(itemDiv).append("\u20AC" + price + " ").append(quantity + " ").append(name + " ").append(sku + " ");
- 					}
- 			total += cartItem.price;
-		 		}	
-			}	
+	 			var skuString = $(".cart-item").children(".sku-text").text();
+
+	 			var priceText = $("<div>").addClass("cart-price").text("$" + price + " x " + quantity);
+	 			var nameText = $("<p>").text(name);
+	 			var skuText = $("<p>").addClass("sku-text").text(sku);
+	 			var itemText = $(itemDiv).append(priceText).append(nameText).append(skuText);
+
+				$("#cart").append(itemText);
+
+				total += quantity * price;
+
+		} //end of for loop
+
+	}
 
 	refreshCart();
 	
-	if ($(".us").is(":visible")){
-		$(".cart-total").text("Total $" + total);
-	} else {
-		$(".cart-total").text("Total \u20AC" + total);
-	}
-});
+		if ($(".us").is(":visible")){
+			$(".cart-total").text("Total $" + total);
+		} else {
+			$(".cart-total").text("Total \u20AC" + total);
+		}
 
+}); //end of on click function
 
 }); //end of jquery
